051bb4705cc6bc8fa627a33f60d62bd1b96e002d
reinans <reinandesouza01@gmail.com>
2022-04-19 14:17:46 -0300

    first commit

---
+++ src/main/java/com/ifba/phonebook_api/PhonebookApiApplication.java
@@ -0,0 +1,13 @@
+package com.ifba.phonebook_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookApiApplication.class, args);
+	}
+
+}
